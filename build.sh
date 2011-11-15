00d8211f37bd4cccb0ef44b021a2b92b87e8243e
wfluro <wfluro@a297bd5c-318b-11de-a996-7ff70a941223>
2011-11-15 12:46:00 +0000

    Updated template and made it much simpler. Updates style files. Cleaned up unised files. Updated build script.
    git-svn-id: https://sdocml.googlecode.com/svn/trunk@158 a297bd5c-318b-11de-a996-7ff70a941223

--- build.sh
+++ build.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
-if [ "$1" == dir ]; then
-    RES=build/sdoc
-else
-    RES=build/sdoc.tar.bz2
-fi
+RES=build/sdoc.tar.bz2
+PYTHON=python
+
+while [ -n "$1" ]; do
+  case "$1" in
+    --dir) 
+        RES=build/sdoc
+        ;;
+    --python) 
+        PYTHON=$2
+        shift
+        ;;
+  esac
+  shift
+done
 
 if  echo '** building reference sections ***************' && \
-    python sdoc/sdoc2xml.py manual/macrorefdoc.xml \
+    $PYTHON sdoc/sdoc2xml.py manual/macrorefdoc.xml \
         -makedoc       build/output/tagsref.xml \
         -macroref      build/output/macroref.xml \
         -macroreftitle "Commonapi Macro Reference" \
         -dtdpath       dtd  \
         -dtdpath       dtd/external && \
     echo '** Building XML ******************************' && \
-    python sdoc/sdoc2xml.py manual/sdocml.xml \
+    $PYTHON sdoc/sdoc2xml.py manual/sdocml.xml \
         -i build/output \
         -o build/output/sdocml-expanded.xml \
         -dtdpath       dtd  \
         -dtdpath       dtd/external \
         -d final=true && \
     echo '** Building HTML *****************************' && \
-    python sdoc/e2html.py build/output/sdocml-expanded.xml \
+    $PYTHON sdoc/e2html.py build/output/sdocml-expanded.xml \
       -config conf/sdoc-html.conf \
       -i . \
       -o $RES \
